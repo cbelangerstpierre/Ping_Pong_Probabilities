dfc56be40329c48b8f9a7938d12bae50ef14abf8
Cédric Bélanger-St-Pierre <cedricbstpierre@yahoo.com>
2021-04-05 16:00:59 -0400

    Created PingPongGame_Initializer.

--- PingPongGames Creator.R
+++ PingPongGames Creator.R
@@ -3,25 +3,13 @@ library(tidyverse)
 
 PointsJean <- c()
 PointsCedric <- c()
-DayOfGames <- 4
+DayOfGames <- 5
 MonthOfGames <- 4
 YearOfGames <- 21
 
 PingPongGames <- PingPongGames %>% 
   
   add_row(Jean = PointsJean, Cedric = PointsCedric, Day = DayOfGames, Month = MonthOfGames, Year = YearOfGames)
-
-PingPongGames
-
-
-
-#Jean <- c(21, 21, 13, 21)
-#Cedric <- c(19, 17, 21, 16)
-#Day <- c(4, 4, 4, 4)
-#Month <- c(4, 4, 4, 4)
-#Year <- c(21, 21, 21, 21)
-
-#PingPongGames <- data.frame(Jean, Cedric, Day, Month, Year)
 
 PingPongGames
 
---
+++ PingPongGames_Initializer.R
@@ -0,0 +1,7 @@
+Jean <- c(21, 21, 13, 21)
+Cedric <- c(19, 17, 21, 16)
+Day <- c(4, 4, 4, 4)
+Month <- c(4, 4, 4, 4)
+Year <- c(21, 21, 21, 21)
+
+PingPongGames <- data.frame(Jean, Cedric, Day, Month, Year)
